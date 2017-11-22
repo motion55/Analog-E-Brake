bd833b12e80cd388dc1be58283b3b0d6c302c175
Butch <butchtabios@live.com>
2017-11-22 09:56:08 +0800

    Move Joystick folder to make it local to this sketch

--- ANALOG_EBRAKE/ANALOG_EBRAKE.ino
+++ ANALOG_EBRAKE/ANALOG_EBRAKE.ino
@@ -5,17 +5,19 @@
 // by AMSTUDIO
 // 20.1.2017
 
-#include <Joystick.h>
+#include "src/Joystick.h"
 
 void setup()
-
-{pinMode(A0, INPUT); 
-   Joystick.begin();}
+{
+  pinMode(A0, INPUT); 
+  Joystick.begin();
+}
 
 const int pinToButtonMap = A0;
 
 void loop()
-
-{int pot = analogRead(A0);
-int mapped = map(pot,0,1023,0,255);
-{Joystick.setThrottle(mapped);}}
+{
+  int pot = analogRead(A0);
+  int mapped = map(pot,0,1023,0,255);
+  Joystick.setThrottle(mapped);
+}
